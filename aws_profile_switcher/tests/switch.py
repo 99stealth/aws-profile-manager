0246af106ff922a27f70a79d6f23bf5810e6c97d
Roman Banakh <banakh.ri@gmail.com>
2020-07-30 23:03:23 +0300

    Added unittest for create_backup_for_default method

--- aws_profile_switcher/tests/switch.py
+++ aws_profile_switcher/tests/switch.py
@@ -35,5 +35,34 @@ class TestCommon(unittest.TestCase):
         expected_output = 'test1'
         self.assertEquals(self.switch.get_defaults_backup(test_data), expected_output)
 
+    def test_create_backup_for_default(self):
+        test_data = {
+                    'default': 
+                        {
+                            'aws_access_key_id': 'AKIAYYYYYYYYYYYYYYYY',
+                            'aws_secret_access_key': 'yYyYyYyYyYyYyYyYyYyYyYyYyYyYyYyYyYyYyYyY'
+                        },
+                    'test1': 
+                        {
+                            'aws_access_key_id': 'AKIAZZZZZZZZZZZZZZZZ',
+                            'aws_secret_access_key': 'zZzZzZzZzZzZzZzZzZzZzZzZzZzZzZzZzZzZzZzZ'}
+                    }
+        expected_output = {
+                    'default': 
+                        {
+                            'aws_access_key_id': 'AKIAYYYYYYYYYYYYYYYY',
+                            'aws_secret_access_key': 'yYyYyYyYyYyYyYyYyYyYyYyYyYyYyYyYyYyYyYyY'
+                        },
+                    'test1': 
+                        {
+                            'aws_access_key_id': 'AKIAZZZZZZZZZZZZZZZZ',
+                            'aws_secret_access_key': 'zZzZzZzZzZzZzZzZzZzZzZzZzZzZzZzZzZzZzZzZ'},
+                    'test2': 
+                        {
+                            'aws_access_key_id': 'AKIAYYYYYYYYYYYYYYYY',
+                            'aws_secret_access_key': 'yYyYyYyYyYyYyYyYyYyYyYyYyYyYyYyYyYyYyYyY'}
+                    }
+        self.assertDictEqual(self.switch.create_backup_for_default(test_data, 'test2'), expected_output)
+
 if __name__ == '__main__':
     unittest.main()
