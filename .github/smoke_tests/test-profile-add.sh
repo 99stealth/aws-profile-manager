fc7916551b17b5cf824a1018d3a775889d0cf67c
Roman Banakh <banakh.ri@gmail.com>
2020-11-22 11:57:41 +0200

    Fix for test

--- .github/smoke_tests/test-profile-add.sh
+++ .github/smoke_tests/test-profile-add.sh
@@ -2,7 +2,7 @@
 
 echo "----------- Testing utility version functionality -----------"
 
-python3 -m aws_profile_manager.run --aws-profile-name=test --aws-access-key-id=AKIAAAAAAAAAAAAAAAAA --aws-secret-access-key=Aa1Aa0az00+AzA/01AzZZZz0Z0z0ZzzZZzZZz0zZ
+python3 -m aws_profile_manager.run profile-add --aws-profile-name=test --aws-access-key-id=AKIAAAAAAAAAAAAAAAAA --aws-secret-access-key=Aa1Aa0az00+AzA/01AzZZZz0Z0z0ZzzZZzZZz0zZ
 
 if [ $? == 0 ]; then 
     echo "[+] Test Passed"
