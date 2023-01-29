2ca01556431d08ac1b01981c6da198dcb5f2fbfb
Roman Banakh <banakh.ri@gmail.com>
2023-01-29 21:48:48 +0200

    Add profile-current smoke test

--- .github/smoke_tests/3_test-profile-edit.sh
+++ .github/smoke_tests/3_test-profile-edit.sh
@@ -17,7 +17,7 @@ echo "--------------------------------------------------------------"
 
 echo "Test #2: Edit profile AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY and AWS_SECURITY_TOKEN"
 
-python3 -m aws_profile_manager.cli profile-edit --aws-profile-name=test2 --aws-access-key-id=ASIAAAAAAAAAAAAAAAAZ --aws-secret-access-key=Aa1Aa0az00+AzA/01AzZZZz0Z0z0ZzzZZzZZz0zA --aws-session-token asdgsdhafshaerhaerhserhafhashagfagwaerg
+python3 -m aws_profile_manager.cli profile-edit --aws-profile-name=default --aws-access-key-id=ASIAAAAAAAAAAAAAAAAZ --aws-secret-access-key=Aa1Aa0az00+AzA/01AzZZZz0Z0z0ZzzZZzZZz0zA --aws-session-token asdgsdhafshaerhaerhserhafhashagfagwaerg
 
 if [ $? == 0 ]; then 
     echo "[+] Test Passed"
